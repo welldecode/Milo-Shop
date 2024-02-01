31b0673d6cca872ed09d1b711a716c8ec854c931
welldecode <wellingtonalfredo550@gmail.com>
2024-02-01 18:30:33 -0300

    ajustando database

--- config/services.php
+++ config/services.php
@@ -29,11 +29,11 @@ return [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ], 
+    ],
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => '/google/callback',
+        'redirect' => env('GOOGLE_REDIRECT_URI')
     ],
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
